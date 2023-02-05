d2fe1f42a3de55a7b3252e4e8869f821b724f345
Listener4031 <21301050221@m.fudan>
2023-02-05 09:04:47 +0800

    test

--- src/Util/error/MxErrorListener.java
+++ src/Util/error/MxErrorListener.java
@@ -7,7 +7,7 @@ import org.antlr.v4.runtime.Recognizer;
 
 public class MxErrorListener extends BaseErrorListener {
 	@Override
-	public void SyntaxError(Recognizer<?, ?> recognizer,
+	public void syntaxError(Recognizer<?, ?> recognizer,
 							Object offendingSymbol,
 							int line, int charPositionInLine,
 							String msg,
